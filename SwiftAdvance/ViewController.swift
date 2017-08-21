8cb8de09316d2f294a14e1e6d28ecbfc6d541ae8
Toseefhusen Khilji <toseefhusen@rangams-Mac-mini-4.local>
2017-08-21 11:04:04 +0530

    Initial Commit

---
+++ SwiftAdvance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftAdvance
+//
+//  Created by Toseefhusen Khilji on 8/21/17.
+//  Copyright © 2017 Toseefhusen Khilji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
